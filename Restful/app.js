2aaf3a476118dbd1cacbb50bb77ea58a9949da4f
sungju <lsj0147@naver.com>
2017-08-02 16:29:23 +0900

    passport

--- Restful/app.js
+++ Restful/app.js
@@ -12,10 +12,6 @@ app.engine('html',require('ejs').renderFile);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended : true
-}));
 app.use(session({
 		secret : '@#@$MYSIGN@$#$',
 		resave : false,
---
+++ passportTest/app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var fs = require("fs");
+var session = require('express-session');
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html',require('ejs').renderFile);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({
+	secret : '!@#$SIGN',
+	resave : false,
+	saveUninitalized : false
+}));
+
+var passport = require("./router/passport")(app);
+
+app.use("/login", require("./router/login")(passport));
+
+app.get('/',function(req,res){
+	var sess = req.user;
+	if (sess == null)
+	{
+		console.log('비로그인');
+	} else if (sess != null)
+	{
+		console.log('id : ' + sess.username);
+	}
+	res.render('index',{'sess' : sess});
+});
+
+var webServer = app.listen(4000,function() {
+	console.log('port 4000 serverOn');
+});
